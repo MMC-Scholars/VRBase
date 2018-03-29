e82c3bff3a1be4548d3b676c50818e1ef3021100
MMC-Scholars <latrunkster@gmail.com>
2018-03-29 11:53:43 -0400

    Outlined files for input events - Michael

---
+++ Source/VRBase/IBaseEntity/EntityEventInput.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EntityEventInput.h"
+
+EntityEventInput::EntityEventInput()
+{
+}
+
+EntityEventInput::~EntityEventInput()
+{
+}
