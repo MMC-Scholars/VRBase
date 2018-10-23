7f0e74cc8abe7e45e6f88c06a488be465f9b4f0a
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2018-10-23 15:25:00 -0400

    initial exit function - Sam

--- Source/VRBase/System/vr_app_switch.cpp
+++ Source/VRBase/System/vr_app_switch.cpp
@@ -1,6 +1,8 @@
 #include "stdio.h"
 
 #define FILENAME_APP_NEXT "vr_app_next.tmp"
+// Office file executable
+#define FILENAME_OFFICE ""
 
 namespace VRAppController {
 	void markNextProgramName(const char* pszName) {
@@ -12,6 +14,10 @@ namespace VRAppController {
 	}
 
 	void exit() {
-
+		FILE* pFile = fopen(FILENAME_APP_NEXT, "w");
+		if (pFile) {
+			fprintf(pFile, FILENAME_OFFICE);
+			fclose(pFile);
+		}
 	}
 }
