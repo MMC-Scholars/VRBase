7fd126ac8128cd770ae1122eca5fa7bddbcac506
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-09-05 16:09:08 -0400

    removed unecessary generated files - Sam

--- Source/VRBase/IBaseEntity/CTakeDamageInfo.h
+++ Source/VRBase/IBaseEntity/CTakeDamageInfo.h
@@ -1,7 +1,9 @@
-// This software is under partial ownership by The Ohio State University, 
-//for it is a product of student employees. For official policy, see
-//https://tco.osu.edu/wp-content/uploads/2013/09/PatentCopyrightPolicy.pdf 
-//or contact The Ohio State University's Office of Legal Affairs
+/**
+ * This software is under partial ownership by The Ohio State University, 
+ * for it is a product of student employees. For official policy, see
+ * https://tco.osu.edu/sites/default/files/pdfs/IP-Policy.pdf 
+ * or contact The Ohio State University's Office of Legal Affairs.
+ */
 
 //-------------------------------------------------------------------------------------
 // The purpose of this file is to declare a class which contains damage information.
@@ -13,15 +15,13 @@
 
 #include "System/predefs.h"
 
-
-
 class CTakeDamageInfo {
 private:
 	ushort m_iBaseDamage;
 	EHANDLE m_hReceiver; //entity who "receives" this attack
 	EHANDLE m_hAttacker;
 	//EHANDLE m_pWeapon;
-	
+
 public:
 	CTakeDamageInfo(ushort damage, EHANDLE receiver, EHANDLE attacker)
 		: m_iBaseDamage(damage), m_hReceiver(receiver), m_hAttacker(attacker) {}
@@ -31,4 +31,4 @@ public:
 	EHANDLE GetAttacker() const { return m_hAttacker; }
 };
 
-#endif //CTAKEDAMAGEINFO_H
+#endif // CTAKEDAMAGEINFO_H
