9eb23f1a3d91cd5aac707a1513ae0a0421bed6cb
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2020-04-04 16:10:56 -0400

    Removed Unnecessary Code in AMoveLinear

--- AMoveLinear/AMoveLinear.cpp
+++ AMoveLinear/AMoveLinear.cpp
@@ -15,26 +15,6 @@ void AMoveLinear::OnConstruction(const FTransform& transform) {
 	SetPositionFromController(NULL);
 }
 
-void AMoveLinear::PreInit() {
-	m_vOriginalDirection = m_vDirection;
-	m_hasParent = GetRootComponent()->GetAttachParent() != nullptr;
-
-	// when attached to another actor, this is not at its start loc
-	if (m_hasParent) {
-		FTransform parentTransform = GetRootComponent()->GetAttachParent()->GetOwner()->GetActorTransform();
-		// vector from parent to original location of this (m_startLoc stays constant)
-		m_startLoc = GetActorLocation() - parentTransform.GetLocation();
-		m_parentToThis = m_startLoc;
-		m_startPlane = FPlane(GetActorLocation(), m_vDirection);
-		m_endPlane = FPlane(GetActorLocation() + m_vDirection, -m_vDirection);		
-	} else {
-		m_startLoc = GetActorLocation();
-		m_startPlane = FPlane(m_startLoc, m_vDirection);
-		m_endPlane = FPlane(m_startLoc + m_vDirection, -m_vDirection);
-	}
-	Super::PreInit();
-}
-
 void AMoveLinear::SetPositionFromController(ABaseController* pController) {
 	// set world location
 	FVector pos = m_pAxis->FindLocationClosestToWorldLocation(m_vTempAttachLoc, ESplineCoordinateSpace::World);
@@ -42,12 +22,4 @@ void AMoveLinear::SetPositionFromController(ABaseController* pController) {
 
 	// set lerp from spline lerp value
 	SetLerpPosition(m_pAxis->FindInputKeyClosestToWorldLocation(pos));
-}
-
-void AMoveLinear::SetLerpPosition(float _lerp) {
-	Super::SetLerpPosition(_lerp);
-}
-
-void AMoveLinear::DefaultThink() {
-	Super::DefaultThink();	
 }
--- AMoveLinear/AMoveLinear.h
+++ AMoveLinear/AMoveLinear.h
@@ -16,19 +16,7 @@ class VRBASE_API AMoveLinear : public ABaseMoving {
 		AMoveLinear();
 		virtual void OnConstruction(const FTransform& transform) override;
 
-		void PreInit() override;
-		void DefaultThink() override;
 		void SetPositionFromController(ABaseController* pController) override;
-		void SetLerpPosition(float _lerp) override;
 
 		USplineComponent*							m_pAxis;
-
-		FVector										m_vDirection;
-		FVector										m_vOriginalDirection;
-
-		FVector										m_parentToThis;
-		FVector										m_startLoc;
-		FPlane										m_startPlane;
-		FPlane										m_endPlane;
-		bool										m_hasParent;
 };
