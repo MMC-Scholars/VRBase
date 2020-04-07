f7361ba460614d019a3018da149aa1f62ad5032c
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2020-04-06 22:22:27 -0400

    Began Logic of Lerp Rotation of the APickup Mesh

--- AMoveRotational/AMoveRotational.cpp
+++ AMoveRotational/AMoveRotational.cpp
@@ -10,42 +10,60 @@ AMoveRotational::AMoveRotational() {
 
 	m_fRotRadius = 50;
 	m_fRotRange = 360;
+
+	m_vRotAxis = m_pAxis->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::Local) - m_pAxis->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::Local);
+	m_vInitialDirection = m_fRotRadius * (m_pAxis->GetRightVectorAtSplinePoint(0, ESplineCoordinateSpace::World) ^ m_vRotAxis);
 }
 
 void AMoveRotational::OnConstruction(const FTransform& transform) {
 	Super::OnConstruction(transform);
 
 	// set mesh initial lerp
-	m_vRotAxis = m_pAxis->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::Local) - m_pAxis->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::Local);
-	m_vInitialDirection = m_fRotRadius * (m_pAxis->GetRightVectorAtSplinePoint(0, ESplineCoordinateSpace::World) ^ m_vRotAxis);
+	// m_vRotAxis = m_pAxis->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::Local) - m_pAxis->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::Local);
+	// m_vInitialDirection = m_fRotRadius * (m_pAxis->GetRightVectorAtSplinePoint(0, ESplineCoordinateSpace::World) ^ m_vRotAxis);
 	m_vTempAttachLoc = m_vInitialDirection.RotateAngleAxis(m_lInitialLerp * m_fRotRange, m_vRotAxis / m_vRotAxis.Size());
 	
 	m_vRangeVector = m_vInitialDirection.RotateAngleAxis(m_fRotRange, m_vRotAxis / m_vRotAxis.Size());
 	
 	// transform mesh into correct initial position
-	FVector vNewAxisLoc = 0.5 * m_vRotAxis + m_fRotRadius * (m_vTempAttachLoc / m_vTempAttachLoc.Size());
+	FVector vNewAxisLoc = 0.5 * m_vRotAxis + m_fRotRadius * (m_vInitialDirection / m_vInitialDirection.Size());
 	FRotator rNewAxisRot = FRotator(m_vRotAxis.Rotation().Pitch - 90, m_vRotAxis.Rotation().Yaw, m_vRotAxis.Rotation().Roll);
 	m_pPickupMeshComponent->SetRelativeLocationAndRotation(vNewAxisLoc, rNewAxisRot);
+	// FRotator rNewAxisRot = FRotator(m_vTempAttachLoc.Rotation().Pitch, m_vTempAttachLoc.Rotation().Yaw, m_vTempAttachLoc.Rotation().Roll);
+	// FRotator rNewAxisRot = (m_vTempAttachLoc - m_pAxis->GetLocationAtDistanceAlongSpline(m_vRotAxis.Size() / 2, ESplineCoordinateSpace::World)).Rotation();
+	// m_pPickupMeshComponent->SetRelativeLocation(vNewAxisLoc);
+	// m_pPickupMeshComponent->SetWorldRotation(rNewAxisRot);
 
 	SetPositionFromController(NULL);
 }
 
 void AMoveRotational::SetPositionFromController(ABaseController* pController) {
 	FVector vControllerDir = FVector::PointPlaneProject(m_vTempAttachLoc, m_vInitialDirection, m_vRotAxis);
-	m_lCurrentLerp = FMath::Acos( (vControllerDir | m_vInitialDirection) / (m_vInitialDirection.Size() * vControllerDir.Size()) ) / abs(m_fRotRange);
+	float fSmallAngle = FMath::Acos( (vControllerDir | m_vInitialDirection) / (m_vInitialDirection.Size() * vControllerDir.Size()) ) * 180 / PI;
+	
+	FVector vRotated = m_vInitialDirection.RotateAngleAxis(fSmallAngle, m_vRotAxis);
+	float fRotAngle = (vControllerDir / vControllerDir.Size()).Equals(vRotated / vRotated.Size(), 1) ? (fSmallAngle) : (360 - fSmallAngle);
 
-	if (abs(m_lCurrentLerp) <= 1) {
-		FRotator rLerpRotation = FMath::Lerp(m_vInitialDirection.Rotation(), m_vRangeVector.Rotation(), m_lCurrentLerp) - m_vInitialDirection.Rotation();
-		// FVector vLerpDirection = m_vInitialDirection.RotateAngleAxis(fRotAngle, m_vRotAxis / m_vRotAxis.Size());
+	m_lCurrentLerp = fRotAngle / abs(m_fRotRange);
+
+	if (m_lCurrentLerp <= 1) {
+		// FRotator rLerpRotation = FMath::Lerp(m_vInitialDirection.Rotation(), m_vRangeVector.Rotation(), m_lCurrentLerp);
+		fSmallAngle = (fRotAngle > 180) ? (-fSmallAngle) : (fSmallAngle);
+ 		FRotator rWrongAxis = m_vInitialDirection.RotateAngleAxis(fSmallAngle, m_vRotAxis / m_vRotAxis.Size()).Rotation();
+		FRotator rLerpRotation = FRotator(0, rWrongAxis.Yaw, rWrongAxis.Pitch);
 		// FRotator rLerpRotation = (FVector::Parallel(vLerpDirection, vControllerDir)) ? (vLerpDirection.Rotation()) : (FRotator(180) - vLerpDirection.Rotation());
 
 		// FRotator rLerpRotation = vControllerDir.Rotation();
 
-		m_pAxis->SetWorldRotation( rLerpRotation + FRotator(90, 0, 0) );
+		m_pAxis->SetWorldRotation( m_vInitialDirection.Rotation() );
+		Msg("%f", fRotAngle);
 	}
 	
-	Msg("Point 1: ");
-	Msg(m_pAxis->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::World));
+	//Msg("Point 1: ");
+	//Msg(m_pAxis->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::World));
+	//float vRotAxisMag = (m_pAxis->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::World) - m_pAxis->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::World)).Size();
+	//if ( (vControllerDir / vControllerDir.Size()).Equals(rotated / rotated.Size(), 1) )
+	//Msg(m_vTempAttachLoc - m_pAxis->GetLocationAtDistanceAlongSpline(vRotAxisMag / 2, ESplineCoordinateSpace::World));
 }
 
 void AMoveRotational::SetLerpPosition(float _lerp) {
