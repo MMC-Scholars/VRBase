85f0e37c946c4e5686dad78b6f31a4d4506641c7
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-04 16:06:54 -0400

    Remove outdated license comments

--- System/static_initialize.h
+++ System/static_initialize.h
@@ -1,10 +1,3 @@
-/**
- * This software is under partial ownership by The Ohio State University,
- * for it is a product of student employees. For official policy, see
- * https://tco.osu.edu/sites/default/files/pdfs/IP-Policy.pdf
- * or contact The Ohio State University's Office of Legal Affairs.
- */
-
 #ifndef STATIC_INITIALIZE_H
 #define STATIC_INITIALIZE_H
 
