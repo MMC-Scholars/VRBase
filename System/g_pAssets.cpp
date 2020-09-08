6baedd942bad055be8d6543bcfe437baa0fbdf61
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2020-09-08 12:05:32 -0400

    Created g_pAssets header & cpp files

---
+++ System/g_pAssets.cpp
@@ -0,0 +1,20 @@
+﻿// This software is under partial ownership by The Ohio State University, for it
+// is a product of student employees. For official policy, see
+// https://tco.osu.edu/sites/default/files/pdfs/IP-Policy.pdf or contact The
+// Ohio State University's Office of Legal Affairs.
+
+#include "Globals.h"
+#include "AGameRules/AGameRules.h"
+#include "NLogger.h"
+
+
+UStaticMesh* GPAssetsVar::FindMesh(const char* path){
+//TODO: Figure out implementation!
+
+}
+
+UStaticMesh# GPAssetsVar::FindMesh(const wchar_t* path){
+
+
+
+}
---
+++ System/g_pAssets.h
@@ -0,0 +1,38 @@
+﻿// This software is under partial ownership by The Ohio State University, for it
+// is a product of student employees. For official policy, see
+// https://tco.osu.edu/sites/default/files/pdfs/IP-Policy.pdf or contact The
+// Ohio State University's Office of Legal Affairs.
+
+#ifndef GLOBALS_ASSETS_H
+#define GLOBALS_ASSETS_H
+
+#include "NLogger.h"
+#include "predefs.h"
+#include <chrono>
+
+using namespace std::chrono;
+
+
+class GPAssetsVar{
+
+public:
+
+    //A function which returns a static mesh, given a path reference to the mesh
+
+    UStaticMesh* FindMesh(const char* path);
+    UStaticMesh* FindMesh(const wchar_t* path);
+
+    //TODO: Add functions for finding material, animation, etc.
+    
+
+
+
+
+};
+
+
+
+
+
+
+#endif // GLOBALS_ASSETS_H
