7e5db68fbd40665be0d20ca520251731c813e36b
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2020-09-30 16:42:18 -0400

    Removing unnecessary include from FMovingVector.h

--- FMovingVector/FMovingVector.h
+++ FMovingVector/FMovingVector.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "System/predefs.h"
-#include "UnrealEngine.h"
+//#include "UnrealEngine.h"
 
 /**
  *-------------------------------------------------------------------------------------
