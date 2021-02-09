e4e9819d4cb79dea508271eee173a0e8bd913675
Sam Bossley <bossley.samuel@gmail.com>
2021-02-09 15:22:19 +0000

    Remove commented BeginPlay

--- AGameBase/AGameBase.h
+++ AGameBase/AGameBase.h
@@ -17,10 +17,6 @@ public:
     GENERATED_BODY()
     AGameBase();
 
-    // UE4 Overrides create a new game flow. This does not use
-    // IBaseEntity::DefaultThink() nor IBaseEntity::Think() because if it
-    // did, the game flow would end up calling itself.
-    // virtual void BeginPlay() override;
     virtual void Tick(float deltaTime) override;
 
 private:
